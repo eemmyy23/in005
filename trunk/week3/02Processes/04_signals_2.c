8aa7d760c8308c38e06535908174c6124fbb5832
emi.puscalau@gmail.com <emi.puscalau@gmail.com@26f02875-2c3b-4c75-ab72-13e380bed02d>
2012-09-12 18:11:48 +0000

    update processes

--- trunk/week3/02Processes/04_signals_2.c
+++ trunk/week3/02Processes/04_signals_2.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 #include <signal.h>
 
 void sigusr1_handler(int num) {
     int i;
 
-    fprintf(stdout, "Handling of signal %d (%s)\n", num, XXXXXX);
+    fprintf(stdout, "Handling of signal %d (%s)\n", num, strsignal(num));
     for (i = 1; i < 4; i++) {
         fprintf(stdout, "%d\n", i);
         sleep(1);
@@ -19,15 +19,14 @@ void sigusr1_handler(int num) {
 
 int main(void) {
     signal(SIGUSR1, sigusr1_handler);
-
     if (fork() == 0) {
         // I am the child
         // Let's send some signals to my parent!
-        kill(XXXXXX, XXXXXX);
+		kill(getppid(), SIGUSR1);
         
         sleep(5);
         
-        kill(XXXXXX, XXXXXX);
+        kill(getppid(), SIGUSR1);
 
     } else {
         while (1) {
