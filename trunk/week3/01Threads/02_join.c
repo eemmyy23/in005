eae8a25642757800d1ed2859224e13c9fe2caf0c
emi.puscalau@gmail.com <emi.puscalau@gmail.com@26f02875-2c3b-4c75-ab72-13e380bed02d>
2012-09-14 07:34:24 +0000

    review 02_join

--- trunk/week3/01Threads/02_join.c
+++ trunk/week3/01Threads/02_join.c
@@ -1,6 +1,7 @@
-/* gcc XXXX.c -o XXXX -lpthread
+/* gcc XXXX.c -o XXXX -lpthread 
 gcc 02_join.c -o 02_join -lpthread
 */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,9 +9,7 @@ gcc 02_join.c -o 02_join -lpthread
 #include <string.h>
 #include <unistd.h>
 
-//XXXXX
 void * fn_thread(void * inutile);
-int number_from_thread;
 
 int main(void) {
     int i;
@@ -23,13 +22,10 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    //pthread_join(thread, XXXXXX);
-		pthread_join(thread, NULL);
-
+    pthread_join(thread, retour);
 
     if (retour != PTHREAD_CANCELED) {
-        //i = (int) XXXXX;
-			 i = (int) number_from_thread;
+        i = (int) *retour;
         fprintf(stdout, "main : value = %d\n", i);
     }
 
@@ -48,9 +44,6 @@ void * fn_thread(void * inutile) {
             break;
         }
     }
-	 /**/fprintf(stdout, "Thread : your number : %s", chaine);
-	 /**/number_from_thread = i;
-
     sleep(1);
     pthread_exit((void*) i);
 }
